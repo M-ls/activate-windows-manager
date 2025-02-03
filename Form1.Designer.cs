951574a5ca7d60561c73e7d3efd14ebdf1761694
执纨纨 <lavenderls097@gmail.com>
2025-02-04 04:10:19 +0800

    update

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace activate_windows_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startClick = new System.Windows.Forms.Button();
+            this.stopClick = new System.Windows.Forms.Button();
+            this.customArguments = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.helpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startClick
+            // 
+            this.startClick.Location = new System.Drawing.Point(35, 36);
+            this.startClick.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.startClick.Name = "startClick";
+            this.startClick.Size = new System.Drawing.Size(146, 91);
+            this.startClick.TabIndex = 0;
+            this.startClick.Text = "开启";
+            this.startClick.UseVisualStyleBackColor = true;
+            this.startClick.Click += new System.EventHandler(this.startClick_Click);
+            // 
+            // stopClick
+            // 
+            this.stopClick.Location = new System.Drawing.Point(220, 36);
+            this.stopClick.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.stopClick.Name = "stopClick";
+            this.stopClick.Size = new System.Drawing.Size(146, 91);
+            this.stopClick.TabIndex = 1;
+            this.stopClick.Text = "关闭";
+            this.stopClick.UseVisualStyleBackColor = true;
+            this.stopClick.Click += new System.EventHandler(this.stopClick_Click);
+            // 
+            // customArguments
+            // 
+            this.customArguments.Location = new System.Drawing.Point(35, 170);
+            this.customArguments.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.customArguments.Name = "customArguments";
+            this.customArguments.Size = new System.Drawing.Size(282, 22);
+            this.customArguments.TabIndex = 2;
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutoPopDelay = 5000;
+            this.toolTip.InitialDelay = 500;
+            this.toolTip.OwnerDraw = true;
+            this.toolTip.ReshowDelay = 5000;
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(356, 170);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(55, 22);
+            this.helpButton.TabIndex = 3;
+            this.helpButton.Text = "help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 338);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.customArguments);
+            this.Controls.Add(this.stopClick);
+            this.Controls.Add(this.startClick);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "activate-windows";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startClick;
+        private System.Windows.Forms.Button stopClick;
+        private System.Windows.Forms.TextBox customArguments;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Button helpButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace activate_windows_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            miscell();
+            this.Resize += MyForm_Resize;
+        }
+        string tips = "参数：\n-p: 预设参数（\"mac\"、\"bsd\"、\"linux\"、\"hurd\"、\"windows\"、\"unix\"、\"deck\"、\"reactos\"、\"m$\"）\n-t: 自定义标题行\n-m: 自定义内容行\n-b: 加粗\n-i: 斜体\n-f: 自定义字体\n-c: 自定义颜色\n-w: 自定义叠加层宽度\n-h: 自定义叠加层高度\n-s: 自定义缩放\n";
+        private async void startClick_Click(object sender, EventArgs e)
+        {
+            await stableActivity("start", customArguments);
+        }
+        private async void stopClick_Click(object sender, EventArgs e)
+        {
+            await stableActivity("stop", customArguments);
+        }
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            toolTip.Show(tips, helpButton, 5000);
+        }
+        private void ShowWindow(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            ShowWindow(sender, e);
+        }
+        private void ExitApplication(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void MyForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false; 
+                this.Hide();
+            }
+        }
+        public void miscell()
+        {
+            ToolTip tooltip = new ToolTip()
+            {
+                OwnerDraw = true // 启用自定义绘制
+            };
+            toolTip.SetToolTip(customArguments, tips);
+            toolTip.SetToolTip(helpButton, tips);
+            toolTip.Draw += ToolTip_Draw;
+
+            // 初始化 NotifyIcon
+            NotifyIcon notifyIcon = new NotifyIcon
+            {
+                Icon = this.Icon, // 设置托盘图标（可以替换为自定义图标）
+                Text = "activate-windows", // 鼠标悬停时显示的提示文本
+                Visible = true
+            };
+            // 创建右键菜单
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("显示窗口", null, ShowWindow);
+            contextMenu.Items.Add("退出", null, ExitApplication);
+            notifyIcon.ContextMenuStrip = contextMenu;
+
+            // 添加双击事件
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+        }
+        private void ToolTip_Draw(object sender, DrawToolTipEventArgs e)
+        {
+            using (Font font = new Font("Consolas", 9, FontStyle.Bold)) // 自定义字体
+            {
+                // 设置背景颜色
+                e.Graphics.FillRectangle(Brushes.Azure, e.Bounds);
+
+                // 绘制文本
+                e.Graphics.DrawString(e.ToolTipText, font, Brushes.Black, e.Bounds);
+            }
+        }
+        static async Task stableActivity(string stable, TextBox customArguments)
+        {
+            try
+            {
+                // 获取当前程序的运行目录
+                string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                // 构建可执行文件的绝对路径
+                string exePath = Path.Combine(exeDirectory, "Resources", "activate-windows-mingw64.exe");
+
+                // 创建 ProcessStartInfo 对象
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,                                            // 可执行文件路径
+                    Arguments = stable == "start" ? customArguments.Text : "-K",   // 传递给可执行文件的参数
+                    RedirectStandardOutput = true,                                 // 重定向标准输出（可选）
+                    RedirectStandardError = true,                                  // 重定向标准错误（可选）
+                    CreateNoWindow = true,                                         // 禁止创建窗口
+                    UseShellExecute = false                                        // 使用操作系统 shell 执行，设为 false 可隐藏窗口
+                };
+
+                // 启动进程
+                using (Process process = Process.Start(processInfo))
+                {
+                    // 读取标准输出（如果需要查看执行结果）
+                    string output = await process.StandardOutput.ReadToEndAsync();
+                    string error = await process.StandardError.ReadToEndAsync();
+
+                    // 等待进程结束
+                    await Task.Run(() => process.WaitForExit());
+
+                    // 输出结果（可选）
+                    Console.WriteLine("Output: " + output);
+                    Console.WriteLine("Error: " + error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // 捕获并处理异常
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
